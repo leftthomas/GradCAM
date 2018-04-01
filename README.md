a115f04369aee00d2b1cd86e24a47ee881317a5d
leftthomas <leftthomas@qq.com>
2018-04-02 02:24:44 +0800

    add test grad cam and add results

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GradCAM
-add GradCAM class
+A PyTorch implementation of Grad-CAM based on ICCV 2017 paper 
+[Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization](https://arxiv.org/abs/1610.02391)
+
+## Requirements
+* [Anaconda](https://www.anaconda.com/download/)
+* PyTorch
+```
+conda install pytorch torchvision -c pytorch
+```
+* opencv
+```
+conda install opencv
+```
+
+## Usage
+```
+python vis_grad_cam.py --image_name dog_and_cat.png --save_name result.png
+optional arguments:
+--image_name              the tested image name [default value is both.png]
+--save_name               saved image name [default value is grad_cam.png]
+```
+
+## Results
+![result](both.png)
+![result](grad_cam.png)
---
+++ vis_grad_cam.py
@@ -0,0 +1,27 @@
+import argparse
+
+import torch
+from PIL import Image
+from torchvision import transforms
+from torchvision.models import vgg19
+
+from gradcam import GradCam
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test Grad-CAM')
+    parser.add_argument('--image_name', default='both.png', type=str, help='the tested image name')
+    parser.add_argument('--save_name', default='grad_cam.png', type=str, help='saved image name')
+
+    opt = parser.parse_args()
+
+    IMAGE_NAME = opt.image_path
+    SAVE_NAME = opt.save_name
+    test_image = (transforms.ToTensor()(Image.open(IMAGE_NAME))).unsqueeze(dim=0)
+    model = vgg19(pretrained=True)
+    if torch.cuda.is_available():
+        test_image = test_image.cuda()
+        model.cuda()
+    grad_cam = GradCam(model)
+    feature_image = grad_cam(test_image).squeeze(dim=0)
+    feature_image = transforms.ToPILImage()(feature_image)
+    feature_image.save(SAVE_NAME)
